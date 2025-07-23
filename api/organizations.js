bebc6c08828d752f19976bc2357ea38d927c38b1
Kode Pratham <prathamkodeyt@gmail.com>
2025-07-23 10:11:23 +0530

    blah blah

--- api/organizations.js
+++ api/organizations.js
@@ -15,7 +15,7 @@ let organizations = [
   }
 ];
 
-module.exports = function handler(req, res) {
+export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -46,4 +46,4 @@ module.exports = function handler(req, res) {
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
-};
+}
