c739ddf08fb1447073d658d23d1782187c345de3
Kode Pratham <prathamkodeyt@gmail.com>
2025-07-25 00:02:47 +0530

    Update page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -12,6 +12,7 @@ export default function TestPage() {
     password: '',
     confirmPassword: ''
   })
+
   const [isLoading, setIsLoading] = useState(false)
   const [message, setMessage] = useState('')
   const router = useRouter()
